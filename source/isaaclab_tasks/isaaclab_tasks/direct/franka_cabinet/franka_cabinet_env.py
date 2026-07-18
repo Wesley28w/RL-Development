d2966b81c14a07b236802b6740cb629f2e22dd3a
WesleyWong-972 <wesleycwong@gmail.com>
2026-07-18 09:21:01 -0700

    Running ema_sr

--- source/isaaclab_tasks/isaaclab_tasks/direct/franka_cabinet/franka_cabinet_env.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/franka_cabinet/franka_cabinet_env.py
@@ -32,7 +32,7 @@ class FrankaCabinetEnvCfg(DirectRLEnvCfg):
     state_space = 0
 
     # reset state curriculum
-    reset_state_curriculum_enabled = False # True
+    reset_state_curriculum_enabled = True # True
 
     # simulation
     sim: SimulationCfg = SimulationCfg(
@@ -163,10 +163,11 @@ class FrankaCabinetEnvCfg(DirectRLEnvCfg):
     success_buffer_size = 64
     prob_exp = 2 # how much we sharpen the probability distribution (1 = No sharpening)
     sampling_ratio = 0.3 # what fraction of resets go to the sample distribution
-    curriculum_dr = 0.00 # how much domain randomization to apply to robot joints
+    curriculum_dr = 0.02 # how much domain randomization to apply to robot joints
     success_rate_alpha = 0.05 # momentum control of success rate movement (pre-calculations)
     greedy_margin = 0.10 # controls the margin between top and second distribution value that enables softmax
     update_dis_percent = 10 # update 
+    
     # policy params
     curriculum_total_iterations = 2500
     controller_enabled = False
