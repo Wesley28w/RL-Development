9ac46466113c44802c73984985ba984215616883
WesleyWong-972 <wesleycwong@gmail.com>
2026-08-03 20:26:32 -0700

    oops doesn't matter but should be set to 0.3

--- source/isaaclab_tasks/isaaclab_tasks/direct/factory/factory_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/factory/factory_env_cfg.py
@@ -88,7 +88,7 @@ class FactoryEnvCfg(DirectRLEnvCfg):
         "fixed_quat",
     ]
 
-    reset_state_curriculum_enabled = True
+    reset_state_curriculum_enabled = False
 
     task_name: str = "peg_insert"  # peg_insert, gear_mesh, nut_thread
     task: FactoryTask = FactoryTask()
@@ -192,7 +192,7 @@ class FactoryEnvCfg(DirectRLEnvCfg):
     # custom hyperparameters
     success_buffer_size = 64
     prob_exp = 2 # how much we sharpen the probability distribution (1 = No sharpening)
-    sampling_ratio = 0.4 # what fraction of resets go to the sample distribution
+    sampling_ratio = 0.3 # what fraction of resets go to the sample distribution
     curriculum_dr = 0.00 # how much domain randomization to apply to robot joints
     success_rate_alpha = 0.05 # momentum control of success rate movement (pre-calculations)
     greedy_margin = 0.10 # controls the margin between top and second distribution value that enables softmax
