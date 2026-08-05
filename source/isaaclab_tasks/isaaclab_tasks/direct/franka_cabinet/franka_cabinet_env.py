1a63af31cc9f6faee654dbb529c875573127db93
WesleyWong-972 <wesleycwong@gmail.com>
2026-08-05 07:16:43 -0700

    testing 0.1 sample rate

--- source/isaaclab_tasks/isaaclab_tasks/direct/franka_cabinet/franka_cabinet_env.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/franka_cabinet/franka_cabinet_env.py
@@ -162,7 +162,7 @@ class FrankaCabinetEnvCfg(DirectRLEnvCfg):
     # custom hyperparamters
     success_buffer_size = 64
     prob_exp = 2 # how much we sharpen the probability distribution (1 = No sharpening)
-    sampling_ratio = 0.3 # what fraction of resets go to the sample distribution
+    sampling_ratio = 0.1 # what fraction of resets go to the sample distribution
     curriculum_dr = 0.02 # how much domain randomization to apply to robot joints
     success_rate_alpha = 0.05 # momentum control of success rate movement (pre-calculations)
     greedy_margin = 0.10 # controls the margin between top and second distribution value that enables softmax
